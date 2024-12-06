a901f45a38bff64d6d33e9901d11c217850e7bc9
Rahul Vaidya <rahulvaidya202@gmail.com>
2024-12-06 14:07:46 +0530

    Improved Dynamic Controller and Action Handling And url like /login/1/2/3 Paramter Handling

---
+++ app/controllers/LoginController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class LoginController
+{
+    public function demo($param1, $param2, $param3)
+    {
+        echo "Demo method called with params: $param1, $param2, $param3";
+    }
+}
--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <?php
 define("APP_Root", dirname(__FILE__));
-require_once APP_Root . '/app/config/config.php';
+define('CONFIG', require_once APP_Root . '/app/config/config.php');
 require_once APP_Root . '/vendor/autoload.php';
+
+session_start();
+
+use App\Core\Route;
+
+
+$router = new Route();
+// Define routes
+// $router->get('/', 'HomeController@index');
+// $router->get('/login/{id}', 'LoginController@login', [LoginController::class, 'demo']);
+
+
+
+// $router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+
+// Define routes with middleware
+$router->addRoute('GET', '/login/{param1}/{param2}/{pa}', [LoginController::class, 'demo'], [
+    // AuthMiddleware::class,
+    // LoggingMiddleware::class
+]);
+
+// Parse the current URL
+$requestUri = $_SERVER['REQUEST_URI']; //strtok($_SERVER['REQUEST_URI'], '?');
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+try {
+    // Resolve the route
+    $router->resolve($requestUri, $requestMethod);
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
